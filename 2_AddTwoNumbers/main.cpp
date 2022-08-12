1dbaa5be1049d4f80cf6928c0a239251b4f997a0
Git-Senin <lawrencecole8@yahoo.com>
2022-08-12 16:25:06 -0700

    add .gitignore, update main

--- 2_AddTwoNumbers/main.cpp
+++ 2_AddTwoNumbers/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+typedef struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+}   ListNode;
+
 int main()
 {
-    system("pause");
+    // ListNode n1(1);
+    // ListNode n2(2);
+    printf("Hi");
+    std::cout << std::endl;
     return 0;
 }
